4643dc3f36746a7aaa9469866258d7577be7052b
Tonyfacio <tonybatistar@mail.com>
2024-06-08 20:53:42 -0400

    Agregar archivo Hola Mundo en C#

---
+++ HolaMundo/hola.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HolaMundo
+{
+    class hola
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Mundo");
+        }
+    }
+}
